1e6fdecd4f9df5f1912478968e8815b5f29e3b7e
eduardomessias <eduardo.messias@outlook.com>
2023-07-22 21:35:10 +0100

    Model implementation

--- django_project/budget/models.py
+++ django_project/budget/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Budget(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Income(models.Model):
+    source = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    date = models.DateField()
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.source
+
+    class Meta:
+        verbose_name_plural = 'Income'
+
+
+class Expense(models.Model):
+    source = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    date = models.DateField()
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.source
+
+    class Meta:
+        verbose_name_plural = 'Expenses'
