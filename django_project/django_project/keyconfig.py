84dbb315a1a43ccb6bdbd2a43fd9399f4d9fdc74
eduardomessias <eduardo.messias@outlook.com>
2023-08-23 20:52:23 +0100

    deploy preparations

---
+++ django_project/django_project/keyconfig.py
@@ -0,0 +1,13 @@
+import os
+
+
+class Database:
+    NAME = os.getenv('POSTGRES_DB')
+    USER = os.getenv('POSTGRES_USER')
+    PASSWORD = os.getenv('POSTGRES_PASSWORD')
+    HOST = os.getenv('DATABASE_HOST')
+    PORT = os.getenv('DATABASE_PORT')
+
+
+class Secrets:
+    SECRET_KEY = '4g#ro@L8ti=|0&*w?38jFXJO;q}F&2'
